73481e009269c419307868ef07afae39eaab43e1
codwerk <louisws@outlook.fr>
2021-11-08 11:12:16 +0100

    init scroll to

--- 01-landing-page/assets/js/index.js
+++ 01-landing-page/assets/js/index.js
@@ -13,3 +13,34 @@ const navToggle = document.querySelector('#nav-toggle');
 navToggle.addEventListener('click', () => {
   t1.reversed(!t1.reversed());
 })
+
+
+/*=============== INIT SCROLLTO ===============*/
+
+function initScrollTo() {
+  
+  gsap.utils.toArray('#header a').forEach( link => {
+    
+    const target = link.getAttribute('href');
+
+    if(!target) {
+      return;
+  }
+
+    link.addEventListener('click', (e)=> {
+      e.preventDefault();
+      t1.reverse();
+      gsap.to(window, {duration: 1.5, scrollTo: target, ease:'Power2.out'})
+    })
+  });
+}
+
+function init() {
+  initScrollTo();
+}
+
+
+
+window.addEventListener('load', function() {
+  init()
+})
