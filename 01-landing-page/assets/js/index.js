efe27ff1c181ebc925b2c64f7ab8ca651e60a2d7
codwerk <louisws@outlook.fr>
2021-11-09 10:26:28 +0100

    end dark mode

--- 01-landing-page/assets/js/index.js
+++ 01-landing-page/assets/js/index.js
@@ -85,6 +85,37 @@ gsap.to('#scrollup', {
   markers: true
 })
 
+/*=============== DARK LIGHT THEME ===============*/
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-toggle-right';
+
+// si sélectionné par l'utilisateur
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// On obtient le thème actuel de l'interface en validant la classe dark-theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+
+// Nous validons si l'utilisateur a préalablement choisi 
+if (selectedTheme) {
+  // Si la validation est remplie, nous demandons quel était le problème pour savoir si nous avons activé ou désactivé le dark 
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activer/désactiver le thème manuellement avec le bouton 
+themeButton.addEventListener('click', () => {
+    // Ajouter ou supprimer le thème sombre/icône
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // Nous sauvegardons le thème et l'icône actuelle que l'utilisateur a choisi 
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
 function init() {
   initScrollTo();
 }
